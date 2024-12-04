419c0e6ece1819bfcc63bf57127417a2abb932d4
Mffff4 <xdmaik.xdfox@gmail.com>
2024-12-04 13:47:03 +0300

    Add Docker environment detection and update window title in main.py
    - Implemented a function to check if the application is running in a Docker container.
    - Updated the set_window_title function to skip title setting when in a Docker environment.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import asyncio
 from contextlib import suppress
 from bot.core.launcher import process
 from os import system, name as os_name
+import os
+
+
+def is_docker():
+    """Check if we're running in a Docker container"""
+    path = '/proc/self/cgroup'
+    return os.path.exists('/.dockerenv') or (os.path.isfile(path) and any('docker' in line for line in open(path)))
 
 
 def set_window_title(title):
@@ -9,6 +16,8 @@ def set_window_title(title):
     Args:
         title (str): New window title
     """
+    if is_docker():
+        return
     if os_name == 'nt':
         system(f'title {title}')
     else:
@@ -20,7 +29,6 @@ async def main():
 
 
 if __name__ == '__main__':
-    # TODO Replace with bot title
-    set_window_title('Blum')
+    set_window_title('WebEmoji')
     with suppress(KeyboardInterrupt):
         asyncio.run(main())
